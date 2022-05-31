30a9d9a7fb67cdbca2438b2c1145406ad5ccccc3
Cristian <cristian@MBPC.local>
2022-05-31 13:03:28 +0200

    Test env variables

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import BusStop from "./pages/BusStop";
 import Home from "./pages/Home";
 
 function App() {
+  const [appId, setAppId] = useState(import.meta.env.VITE_APP_ID_TFL);
+  console.log(import.meta.env.VITE_APP_ID_TFL);
   return (
     <div className="App">
       <Link href="/" className="active">
@@ -16,6 +18,7 @@ function App() {
       ;
       <Route path="/" component={Home} />
       <Route path="/bus-stop/:id">{BusStop}</Route>
+      {appId}
     </div>
   );
 }
